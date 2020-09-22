35c650d2876d9b857ebe7444e1ccb96e475dbd5b
Hemanth <hemanth.kotagiri43@gmail.com>
2020-09-23 03:17:34 +0530

    Add show_all_elements methods for both Stack and Queue and add the ability to choose between them in main

--- ds/linked_list/src/ll.c
+++ ds/linked_list/src/ll.c
@@ -1,53 +1,88 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-struct Node {
+struct StackNode {
     int data;
-    struct Node* next;
+    struct StackNode* next;
 };
 
 // Stack Implementation
 
-struct Node* top = NULL;
+struct StackNode* tail = NULL;
 
-void push(int value){
-    struct Node* new = (struct Node*)malloc(sizeof(struct Node));
+void stack_push(int value) {
+    struct StackNode* new = (struct StackNode*)malloc(sizeof(struct StackNode));
     new->data = value;
-    new->next = top;
-    top = new;
-    printf("\nYayayayayayayayaya pushed!!!\n\n");
+    new->next = tail;
+    tail = new;
 }
 
+int stack_pop() {
+    if(tail == NULL) {
+        return 0;
+    }
+    int popped_value = tail->data;
+    tail = tail->next;
+    printf("The value %d was popped!\n", popped_value);
+    return popped_value;
+}
 
-void pop(){
-    if(top == NULL){
+void stack_peek() {
+    if(tail == NULL) {
         return;
     }
-    top = top->next;
-    printf("DONE!\n");
+    printf("%d\n", tail->data);
 }
 
-void peek(){
-    if(top == NULL){
-        return;
-    }
+void stack_show_all_elements() {
+    if(tail == NULL) return;
 
-    printf("%d\n", top->data);
+    struct StackNode* temp;
+    temp = tail;
 
-}
-
-void showAllElements(){
-    if(top == NULL) return;
-
-    struct Node* temp;
-    temp = top;
-
-    while(temp != NULL){
+    while(temp != NULL) {
         printf("%d ", temp->data);
         temp = temp -> next;
     }
     printf("\n");
+}
 
+void stack_prompt() {
+    int value, given;
+    printf("1 => Push a value in Stack\n");
+    printf("2 => Peek the top of the stack\n");
+    printf("3 => Pop an element from Stack\n");
+    printf("4 => Display all elements in Stack\n");
+    printf("5 => Exit\n");
+    do {
+        printf(">>> ");
+        scanf("%d", &given);
+        switch(given) {
+            case 1: {
+                printf("Value: ");
+                scanf("%d", &value);
+                stack_push(value);
+                break;
+            }
+            case 2: {
+                stack_peek();
+                break;
+            }
+            case 3: {
+                stack_pop();
+                break;
+            }
+            case 4: {
+                stack_show_all_elements();
+            }
+            case 5: {
+                break;
+            }
+            default: {
+                printf("Not a valid input!\n");
+            }
+        }
+    } while(given!=5);
 }
 
 //
@@ -55,6 +90,10 @@ void showAllElements(){
 //
 // Should be pretty straight foward to understand
 //
+struct Node {
+    int data;
+    struct Node* next;
+};
 
 struct Queue {
     struct Node* head;
@@ -95,50 +134,67 @@ int queue_pop(struct Queue* list) {
 
     int val = head->data;
     free(head);
+    printf("The value %d was popped.\n", val);
     return val;
 }
 
-int main() {
-    // Testing the stack implementation
+void queue_show_all_elements(struct Queue* list) {
+    if (list->size == 0) {
+        return;
+    }
+    struct Node* temp_pointer = list->head;
+    while(temp_pointer != NULL) {
+        printf("%d ", temp_pointer->data);
+        temp_pointer = temp_pointer->next;
+    }
+    printf("\n");
+}
 
+void queue_prompt() {
     int value, given;
-    printf("1 => Push a value in Stack\n");
-    printf("2 => Peek the top of the stack\n");
-    printf("3 => Pop an element from Stack\n");
-    printf("4 => Display all elements in Stack\n");
-    printf("5 => Exit\n");
-
-    do{
+    printf("1 => Queue Push\n");
+    printf("2 => Queue Pop\n");
+    printf("3 => Show all Elements in Queue\n");
+    printf("4 => Exit\n");
+    // Creating our Queue
+    struct Queue* our_queue = create_queue();
+    do {
         printf(">>> ");
         scanf("%d", &given);
-
-        switch(given){
-            case 1:
+        switch(given) {
+            case 1: {
                 printf("Value: ");
                 scanf("%d", &value);
-                push(value);
+                queue_push(our_queue, value);
                 break;
-            
-            case 2:
-                peek();
+            }
+            case 2: {
+                queue_pop(our_queue);
                 break;
-
-            case 3:
-                pop();
+            }
+            case 3: {
+                queue_show_all_elements(our_queue);
+            }
+            case 4: {
                 break;
-            
-            case 4:
-                showAllElements();
-
-            case 5:
-                break;
-            
-            default:
-                printf("Not a valid input!");
+            }
+            default: {
+                printf("Not a valid input!\n");
+            }
         }
-    }
-    while(given!=5);
+    } while(given!=4);
 
+}
+
+int main() {
+    int choice;
+    printf("Queue or Stack:(1/0): ");
+    scanf("%d", &choice);
+    if(choice == 1) {
+        queue_prompt();
+    }else if(choice == 0){
+        stack_prompt();
+    }
     return 0;
 }
 
