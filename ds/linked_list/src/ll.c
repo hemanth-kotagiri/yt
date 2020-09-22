2148e4d3b8b92e0104e454d27151725d9f30d563
Hemanth <hemanth.kotagiri43@gmail.com>
2020-09-23 00:59:14 +0530

    Add Stack Implementation in C

--- ds/linked_list/src/ll.c
+++ ds/linked_list/src/ll.c
@@ -1,14 +1,60 @@
 #include <stdlib.h>
+#include <stdio.h>
+
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+// Stack Implementation
+
+struct Node* top = NULL;
+
+void push(int value){
+    struct Node* new = (struct Node*)malloc(sizeof(struct Node));
+    new->data = value;
+    new->next = top;
+    top = new;
+    printf("\nYayayayayayayayaya pushed!!!\n\n");
+}
+
+
+void pop(){
+    if(top == NULL){
+        return;
+    }
+    top = top->next;
+    printf("DONE!\n");
+}
+
+void peek(){
+    if(top == NULL){
+        return;
+    }
+
+    printf("%d\n", top->data);
+
+}
+
+void showAllElements(){
+    if(top == NULL) return;
+
+    struct Node* temp;
+    temp = top;
+
+    while(temp != NULL){
+        printf("%d ", temp->data);
+        temp = temp -> next;
+    }
+    printf("\n");
+
+}
 
 //
 // QUEUE implementation only.
 //
 // Should be pretty straight foward to understand
 //
-struct Node {
-    int data;
-    struct Node* next;
-};
 
 struct Queue {
     struct Node* head;
@@ -53,5 +99,46 @@ int queue_pop(struct Queue* list) {
 }
 
 int main() {
+    // Testing the stack implementation
+
+    int value, given;
+    printf("1 => Push a value in Stack\n");
+    printf("2 => Peek the top of the stack\n");
+    printf("3 => Pop an element from Stack\n");
+    printf("4 => Display all elements in Stack\n");
+    printf("5 => Exit\n");
+
+    do{
+        printf(">>> ");
+        scanf("%d", &given);
+
+        switch(given){
+            case 1:
+                printf("Value: ");
+                scanf("%d", &value);
+                push(value);
+                break;
+            
+            case 2:
+                peek();
+                break;
+
+            case 3:
+                pop();
+                break;
+            
+            case 4:
+                showAllElements();
+
+            case 5:
+                break;
+            
+            default:
+                printf("Not a valid input!");
+        }
+    }
+    while(given!=5);
+
+    return 0;
 }
 
